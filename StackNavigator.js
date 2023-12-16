1fec3094c89780fe50caba1c586e0eec800258ef
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2023-12-16 21:34:57 +0200

    51:45 Building the Login Functionality (2/2)

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,11 +3,12 @@ import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import ChatScreen from "./screens/ChatScreen";
 import LoginScreen from "./screens/LoginScreen";
+import useAuth from "./hooks/useAuth";
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-    const user = true;
+    const {user} = useAuth();
 
     return (
         <Stack.Navigator>
