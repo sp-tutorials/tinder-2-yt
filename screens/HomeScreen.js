febd4817f5637c0593c7e9c3aac40cf71dcbb072
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2023-12-16 21:27:33 +0200

    34:28 Building the Home Screen (1/2)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
+
     return (
         <View>
             <Text>I am the HomeScreen</Text>
+            <Button title="Go to Chat Screen" onPress={() => navigation.navigate('Chat')}></Button>
         </View>
     );
 }
