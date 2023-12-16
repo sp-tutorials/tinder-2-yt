4b94675d9b6bf86b5b7695303ea20707b8a68ccf
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2023-12-16 21:14:21 +0200

    18:26 Setting up Tailwind React Native (used alternative method w/ package twrnc)

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, Text, View} from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={tw`flex-1 justify-center items-center`}>
+            <Text>Hello, X!</Text>
+            <Button title='Click me'/>
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
