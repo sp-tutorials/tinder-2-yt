d41a3ff68d7af6635f8030f13c395caabd22415a
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2023-12-16 23:15:48 +0200

    1:13:00 -1:19:45

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,11 +1,27 @@
 import React, {createContext, useContext} from 'react';
+import * as Google from "expo-auth-session/providers/google";
 
 const AuthContext = createContext({})
 
+const config = {
+    androidClientId: "817141732167-sg5ut34mkbhb23h9cnb1uqb5jssa4mvb.apps.googleusercontent.com",
+    scopes: ['profile', 'email'],
+    permissions: ["public_profile", "email", "gender", "location"],
+}
+
 export const AuthProvider = ({ children }) => {
+    const [request, response, promptAsync] = Google.useAuthRequest(config);
+    const signInWithGoogle = async () => {
+        promptAsync().then(logInResult => {
+            if (logInResult.type === 'success') {
+                // login...
+            }
+        });
+    }
     return (
         <AuthContext.Provider value={{
-            user: 'Sonny',
+            user: null,
+            signInWithGoogle,
         }}>
             {children}
         </AuthContext.Provider>
