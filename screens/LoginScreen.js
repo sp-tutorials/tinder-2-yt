dadf93b19c73bb30935097b6d1cbfbcf168246d6
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-01-06 19:47:29 +0200

    1:54:32 Building the Login Screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import {View, Text, Button, ImageBackground, TouchableOpacity} from 'react-native';
 import useAuth from "../hooks/useAuth";
 import useFakeLogin from "../hooks/useFakeLogin";
+import {useNavigation} from "@react-navigation/native";
+import tw from 'twrnc';
 
 const LoginScreen = () => {
     const { signInWithGoogle, loading } = useAuth();
+    const navigation = useNavigation();
     const {fakeLoginSuccess, setFakeLoginSuccess} = useFakeLogin();
 
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        });
+    }, []);
+
     return (
-        <View>
-            <View>
-                <Text>{loading ? 'loading...' : "Login to the app"}</Text>
-                <Button title='login' onPress={signInWithGoogle}/>
-            </View>
-            <View>
-                <Text>Fake login success</Text>
-                <Button title={fakeLoginSuccess ? 'Enabled' : 'Disabled'}
-                        onPress={() => setFakeLoginSuccess(!fakeLoginSuccess)}></Button>
-            </View>
+        // <View style={tw`flex-1`}>
+        <View style={tw`flex-1`}>
+            <ImageBackground
+                resizeMode="cover"
+                style={tw`flex-1`}
+                source={{ uri: "https://tinder.com/static/tinder.png" }}>
+                <TouchableOpacity
+                    style={[tw`absolute bottom-40 w-52 bg-white p-4 rounded-2xl`, { marginHorizontal: "25%" }]}
+                    onPress={signInWithGoogle}>
+                    <Text style={tw`font-semibold text-center`}>Sign in & get swiping</Text>
+                </TouchableOpacity>
+            </ImageBackground>
+            <Button title={"Fake login success: " + (fakeLoginSuccess ? 'Enabled' : 'Disabled')}
+                    onPress={() => setFakeLoginSuccess(!fakeLoginSuccess)}></Button>
         </View>
     );
 }
