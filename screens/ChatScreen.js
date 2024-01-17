db1d76d3121bd7dc7d3a726a735a444e88529b48
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-01-17 19:28:59 +0200

    4:18:36 Building the Chat Screen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView} from "react-native-safe-area-context";
+import Header from "../components/Header";
 
 const ChatScreen = () => {
     return (
-        <View>
-            <Text>I am the ChatScreen</Text>
-        </View>
+        <SafeAreaView>
+            <Header title="Chat"/>
+        </SafeAreaView>
     );
 }
 
